52f983d81062f1fd38ae67a2056263c9069f12db
makbu324 <angusmak@bu.edu>
2023-10-10 12:08:53 -0400

    text action added

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.annotation.RequiresApi
@@ -15,16 +17,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toggleLight: SwitchMaterial
     private var cameraManager: CameraManager? = null
     private var getCameraId: String? = null
+
+    //Mak
+    private lateinit var enterButton: Button
+    private lateinit var enterText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toggleLight = findViewById(R.id.switch1)
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+        //Mak
+        enterButton = findViewById(R.id.enter_button)
+        enterText = findViewById(R.id.enter_action)
+
         toggleLight.setOnCheckedChangeListener{ _, _ ->
         toggleFlashlight()
         }
         getCameraId = cameraManager!!.cameraIdList[0]
+
+        //Mak
+        enterButton.setOnClickListener {
+            if (enterText.text.toString() == "on") {
+                toggleLight.isChecked = true
+                toggleFlashlight()
+            } else if (enterText.text.toString() == "off") {
+                toggleLight.isChecked = false
+                toggleFlashlight()
+            } else
+                Toast.makeText(this, "Please type a VALID action: on/off", Toast.LENGTH_SHORT).show()
+            enterText.text.clear()
+        }
     }
 
     // Need to add error checking - phone doesnt have a camera, phone is outdated and cannot
