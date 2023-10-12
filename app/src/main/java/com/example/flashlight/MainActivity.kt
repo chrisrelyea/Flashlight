8c36e83a19a5c32a91450ec98e66f09c147a045c
chrisrelyea <crelyea@bu.edu>
2023-10-12 13:14:50 -0400

    Added fling detection, need to tweak

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -6,15 +6,21 @@ import android.hardware.camera2.CameraManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.View
+import android.util.Log
+import kotlin.math.abs
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.view.MotionEvent
+
 import android.widget.ToggleButton
 import androidx.annotation.RequiresApi
 import com.google.android.material.switchmaterial.SwitchMaterial
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+    private lateinit var gestureDetector: GestureDetector
     private lateinit var toggleLight: SwitchMaterial
     private var cameraManager: CameraManager? = null
     private var getCameraId: String? = null
@@ -27,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toggleLight = findViewById(R.id.switch1)
+        gestureDetector = GestureDetector(this)
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         //Mak
@@ -73,6 +80,81 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
 
 
+    override fun onDown(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent) {
+        return
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent) {
+        return
+    }
+
+
+    // When a fling is detected, some geometry is used to detect swipe direction
+    // This direction of the swipe is stored as "direction" variable
+    override fun onFling(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        Log.d("test", "Fling Detected!")
+
+
+        var x1 = p0.x
+        var y1 = p0.y
+        var x2 = p1.x
+        var y2 = p1.y
+        var diffY = y2-y1
+        var diffX = x2-x1
+        var direction = ""
+
+        direction = if (abs(diffY) > abs(diffX)) {
+            if (y2 >= y1) {
+                "DOWN"
+            } else {
+                "UP"
+            }
+        } else {
+            if (x1 >= x2) {
+                "LEFT"
+            } else {
+                "RIGHT"
+            }
+        }
+        Log.d("direction", direction)
+        var result = false
+        if (direction == "UP"){
+            toggleLight.isChecked = true
+            toggleFlashlight()
+
+        }
+        else if (direction == "DOWN") {
+            toggleLight.isChecked = false
+            toggleFlashlight()
+        }
+
+
+
+        return true
+
+    }
 }
+
+
